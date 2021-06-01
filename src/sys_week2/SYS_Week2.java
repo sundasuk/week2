783051640b3029465f6fc40251d5ffe7d7d396b2
Sukan PC <Sukan PC@LAPTOP-KKGDMTNP>
2021-06-01 14:11:32 -0400

    new repo for week2

---
+++ src/sys_week2/SYS_Week2.java
@@ -0,0 +1,22 @@
+/*
+ * Your Sukan Sundaramoorthi
+ * Student ID: 991 598 890
+ * SYST17796
+ * Date :06/01/2021
+ */
+package sys_week2;
+
+/**
+ *
+ * @author Sukan PC
+ */
+public class SYS_Week2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
